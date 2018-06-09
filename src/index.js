2189104cdcefe405f8b4020fffa93db6e5519ed2
Joshua Thornton <joshua.thornton@heg.com>
2018-06-09 20:25:26 +0100

    feat: expose `createSimpleTag` to public api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import parser from './parser';
 import createAttributeTag from './createAttributeTag';
+import createSimpleTag from './createSimpleTag';
 
 export {
   parser,
   createAttributeTag,
+  createSimpleTag,
 };
